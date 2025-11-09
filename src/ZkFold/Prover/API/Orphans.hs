1a92d4408219c515ff3e203747a39c7e8e93ed31
Eugene Samodelov <geli1610@yandex.ru>
2025-11-09 23:26:29 +0300

    fix ToSchema instance

--- src/ZkFold/Prover/API/Orphans.hs
+++ src/ZkFold/Prover/API/Orphans.hs
@@ -13,7 +13,7 @@ import Data.Swagger qualified as Swagger
 import Data.Swagger.Internal.Schema (named)
 import Deriving.Aeson
 import Servant
-import ZkFold.Prover.API.Types.Prove (ProofStatus)
+import ZkFold.Prover.API.Types.Prove (ProofStatus, ZKProveResult)
 import ZkFold.Prover.API.Utils (addSwaggerDescription)
 import ZkFold.Prover.API.Types.Status (Status)
 
@@ -42,4 +42,4 @@ instance (Swagger.ToSchema o) => Swagger.ToSchema (ProofStatusSchema o) where
             & addSwaggerDescription "Status of a submitted proof"
 
 instance (Swagger.ToSchema o) => Swagger.ToSchema (ProofStatus o) where
-    declareNamedSchema _ = declareNamedSchema (Proxy :: Proxy (ProofStatusSchema o))
+    declareNamedSchema _ = declareNamedSchema (Proxy :: Proxy (ProofStatusSchema (ZKProveResult o)))
